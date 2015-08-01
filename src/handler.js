ae6ab64879748fa198e2ee3fa343b1128f98c4a1
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-08-02 01:04:46 +0200

    Added logs in debug mode

--- src/handler.js
+++ src/handler.js
@@ -8,16 +8,37 @@ var kinesis = new Kinesis({
 });
 
 export default function handler (event, context) {
-    console.log(new Buffer(event.Records[0].kinesis.data, "base64").toString());
     var params = {
         Data: new Buffer(event.Records[0].kinesis.data, "base64").toString(),
         PartitionKey: "pipe",
         StreamName: process.env.TARGET_STREAM_NAME
     };
+    if (process.env.DEBUG) {
+        console.log([
+            "kinesis.putRecord params:",
+            JSON.stringify(params, null, 4)
+        ].join("\n"));
+    }
     return kinesis.putRecord(params, function (err, res) {
         if (err) {
+            if (process.env.DEBUG) {
+                console.log([
+                    "kinesis.putRecord FAILED",
+                    "error.message:",
+                    err.message,
+                    "error.stack:",
+                    err.stack
+                ].join("\n"));
+            }
             context.fail(err);
         } else {
+            if (process.env.DEBUG) {
+                console.log([
+                    "kinesis.putRecord SUCCEEDED",
+                    "response:",
+                    JSON.stringify(res, null, 4)
+                ].join("\n"));
+            }
             context.succeed(res);
         }
     });
